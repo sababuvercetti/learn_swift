f636a77b99f8511f384d5c3e7f68918c45f80f80
Samuel Baraka <sababuvercetti@gmail.com>
2022-07-15 15:38:29 +0300

    Initial Commit

---
+++ learn_swift/learn_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  learn_swiftApp.swift
+//  learn_swift
+//
+//  Created by Samuel Baraka on 15/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct learn_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
