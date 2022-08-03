a84cd1acb2f73614291635021638c09b931dbb96
Samuel Baraka <sababuvercetti@gmail.com>
2022-08-03 14:03:00 +0300

    Learnt about animations

---
+++ Landmarks/Landmarks/Views/Badges/RotatedBadgeView.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeView.swift
+//  Landmarks
+//
+//  Created by Samuel Baraka on 02/08/2022.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
